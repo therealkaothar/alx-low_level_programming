e34493d5aa1a023c6964c66f19fb9e0a25968945
therealkaothar <rajiodunola93@gmail.com>
2023-09-13 05:47:35 -0700

    2-alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,17 +6,16 @@
  */
 void print_alphabet_x10(void)
 {
-        int n;
-        char ch;
-
-        n = 0;
-        while (n < 10)
-        {
-                for (ch = 'a'; ch <= 'z'; ch++)
-                {
-                        _putchar(ch);
-                }
-                n += 1;
-                _putchar('\n');
-        }
+	int n;
+	char ch;
+		n = 0;
+	while (n < 10)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		n += 1;
+		_putchar('\n');
+	}
 }
